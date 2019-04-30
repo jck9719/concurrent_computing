ef78f5804955a948e843b4670973c41276f4fd43
Juliusz <gaunt1111@gmail.com>
2019-04-30 12:19:26 +0200

    lab 8 check

---
+++ 8/main.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(){
+	
+	struct sockaddr_in ip4addr;
+	bzero((char *) &ip4addr, sizeof(ip4addr));
+	ip4addr.sin_family = AF_INET;
+	ip4addr.sin_port = htons((ushort) 5000);
+	inet_pton(AF_INET, "153.19.1.202", &ip4addr.sin_addr);
+
+	int fromlen;
+	fromlen = sizeof(struct sockaddr_in);
+
+	int msg;
+	printf("Podaj liczbe: ");
+	scanf("%d", &msg);
+
+	int s;
+	s = socket(PF_INET, SOCK_DGRAM, 0);
+
+	bind(s, (struct sockaddr*) &ip4addr, fromlen);
+
+	int buff = htonl(msg);
+	sendto(s, (char *)&buff, sizeof(buff), 0, (struct sockaddr*) &ip4addr, fromlen);
+
+	printf("Liczba wyslana do serwera: [%d]\n", msg);
+
+	int byte_count;
+	byte_count = recvfrom(s, (char *)&buff, sizeof(buff), 0, (struct sockaddr*) &ip4addr, &fromlen);
+	buff = ntohl(buff);
+	
+	printf("Liczba otrzymana po obliczeniu przez funkcje na serwerze: %d\n", buff);
+}           
